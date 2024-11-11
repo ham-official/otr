fbf918045f2f4d547f4e714510b7c1c251f536fc
0xdeployer <ethnftboi@protonmail.com>
2024-11-11 10:56:31 -0800

    adding broadcast

--- cli/deploy-tipping-coin.sh
+++ cli/deploy-tipping-coin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .env
 
-forge script script/DeployTippy.s.sol:Run --rpc-url $HAM_RPC -vvvv --chain-id 5112 
+forge script script/DeployTippy.s.sol:Run --rpc-url $HAM_RPC -vvvv --chain-id 5112 --broadcast
